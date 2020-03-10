c7bc0939e7576763664bacda984d167a9f4f6b30
ajnaduvil <ajnaduvil@gmail.com>
2020-03-10 21:54:03 +0530

    added package files

---
+++ README.md
@@ -0,0 +1,9 @@
+# AVR Utils
+A Collection of useful utility scripts for unity testing using test runner
+### Prerequisites
+Unity 2019.3 or above
+### Getting Started
+The project can be imported using pacakge manager. Add ```https://github.com/ajnaduvil/AVR.TestUtils.git``` link to unity package manager and it will be imported into your project.
+![alt text](https://github.com/ajnaduvil/AVR.Utils/blob/master/Documentation/Images/Insert%20from%20package%20manager.png?raw=true)
+
+### Contents
---
+++ Runtime/SceneUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AVR.TestUtils
+{
+    public static class SceneUtils
+    {
+        /// <summary>
+        /// Loads the scene with the name <paramref name="sceneName"/> additively. The scene must be added in build settings
+        /// </summary>
+        /// <param name="sceneName"></param>
+        /// <returns></returns>
+        public static IEnumerator LoadSceneAdditive(string sceneName)
+        {
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+
+        /// <summary>
+        /// Unloads the scene with the name <paramref name="sceneName"/> additively. The scene must be loaded already
+        /// </summary>
+        /// <param name="sceneName"></param>
+        /// <returns></returns>
+        public static IEnumerator UnloadSceneAsync(string sceneName)
+        {
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+    }
+}
